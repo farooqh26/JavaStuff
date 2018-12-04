f5590d7064d9d5c4c36e3aa2c25625646a257bfd
farooq.hussain <farooqh26@gmail.com>
2018-12-04 16:12:41 -0500

    qwqw

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -5,7 +5,12 @@ public class Main {
         for(int i=2;i<9;i++){
             System.out.println("10,000 at " + i +"% interest = " + String.format("%.2f",calculateInterest(10000,i)));
         }
+        System.out.println("-----------------------------------------------------------------------");
+        for(int i=8;i>=2;i--){
+            System.out.println("10,000 at " + i +"% interest = " + String.format("%.2f",calculateInterest(10000,i)));
+        }
     }
+
 
     public static double calculateInterest(double amount, double interestRate){
         return(amount*(interestRate/100));
