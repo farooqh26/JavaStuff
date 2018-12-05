8cf461e0bb67b22b0099a9e2caa7814d77dbbecb
farooq.hussain <farooqh26@gmail.com>
2018-12-05 14:37:14 -0500

    qwqw

--- ForLoop/src/Main.java
+++ ForLoop/src/Main.java
@@ -9,10 +9,35 @@ public class Main {
         for(int i=8;i>=2;i--){
             System.out.println("10,000 at " + i +"% interest = " + String.format("%.2f",calculateInterest(10000,i)));
         }
+        System.out.println("-----------------------------------------------------------------------");
+
+        int count =3;
+        for(int j=0;j<=100;j++){
+            if(isPrime(j)) {
+                System.out.println("Prime number is : " + j);
+            }
+            count = count -1;
+        }
+
     }
+
 
 
     public static double calculateInterest(double amount, double interestRate){
         return(amount*(interestRate/100));
     }
+//-----------------------------------------------------------------------------------//
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for (int i=2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
